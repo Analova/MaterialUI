9d420deedc958af0acda9c3085a5471e54daac07
Ana Nikolova <ani.nik19@gmail.com>
2021-08-04 20:29:21 +0200

    second commit

--- material/src/App.js
+++ material/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { Typography, AppBar, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"
+import { PhotoCamera } from "@material-ui/icons"
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" pharagraph>
+              Hello everyone. This is the best site in the whole universe. I really hope you
+              enjoy it as much I as do. Hope to see you very soon in one of our shops.
+              And I am trying to make this sentence as long as I can. Pleae be patient with me.
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 }
 
