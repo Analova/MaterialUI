183c27961279cd21efe0c8343502fd0b05063eff
Ana Nikolova <ani.nik19@gmail.com>
2021-08-08 13:10:42 +0200

    buttons

--- material/src/App.js
+++ material/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { Typography, AppBar, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"
+import { Typography, Button, AppBar, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"
 import { PhotoCamera } from "@material-ui/icons"
 
 
@@ -27,6 +27,20 @@ const App = () => {
               enjoy it as much I as do. Hope to see you very soon in one of our shops.
               And I am trying to make this sentence as long as I can. Pleae be patient with me.
             </Typography>
+            <div>
+              <Grid container spacing={2} justify="center">
+                <Grid item >
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item >
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
       </main>
