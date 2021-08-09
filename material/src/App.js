f3852783170cf8347de75bb59816f16228c4da81
Ana Nikolova <ani.nik19@gmail.com>
2021-08-09 18:38:05 +0200

    done

--- material/src/App.js
+++ material/src/App.js
@@ -1,24 +1,30 @@
 
 import './App.css';
-import { Typography, Button, AppBar, Card, CardAction, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"
+import { Typography, Button, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"
 import { PhotoCamera } from "@material-ui/icons"
+import useStyles from "./styles";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
 
 
 const App = () => {
+  const classes = useStyles()
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon} />
           <Typography variant="h6">
             Photo Album
           </Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <div>
-          <Container maxWidth="sm">
+        <div className={classes.container}>
+          <Container maxWidth="sm" >
             <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
               Photo Album
             </Typography>
@@ -27,7 +33,7 @@ const App = () => {
               enjoy it as much I as do. Hope to see you very soon in one of our shops.
               And I am trying to make this sentence as long as I can. Pleae be patient with me.
             </Typography>
-            <div>
+            <div className={classes.buttons}>
               <Grid container spacing={2} justify="center">
                 <Grid item >
                   <Button variant="contained" color="primary">
@@ -43,7 +49,43 @@ const App = () => {
             </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://th.bing.com/th/id/OIP.77Cxz_Ts-SsoqSJ8JN5KQAHaEo?pid=ImgDet&rs=1"
+                    title="Image title" />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card, you can use this section to describe the content
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">View</Button>
+                    <Button size="small" color="primary">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))
+            }
+          </Grid>
+        </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography variant="h5" align="center" gutterButton>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondory" >
+          This is a very long description now
+        </Typography>
+
+      </footer>
     </>
   );
 }
